9c0a8cf6869ea3fb1682a37e8acc01840b81c245
Christopher Kelling <cj.kelling88@gmail.com>
2019-12-20 10:48:25 -0700

    Update test to reflect new information found in search results

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -11,12 +11,12 @@ describe "As a user" do
 
       expect(current_path).to eq(search_path)
 
-      expect(page).to have_content('Station Name')
-      expect(page).to have_content('Station Address')
-      expect(page).to have_content('Station Fuel Type')
-      expect(page).to have_content('Station Access Times')
+      expect(page).to have_content('Station Name: Seventeenth Street Plaza')
+      expect(page).to have_content('Station Address: 1225 17th St.')
+      expect(page).to have_content('Station Fuel Type: ELEC')
+      expect(page).to have_content('MO: Not Specified; TU: Not Specified; WE: Not Specified; TH: Not Specified; FR: Not Specified; SA: Not Specified; SU: Not Specified')
 
-      expect(page).to have_content('Distance is 0.1 miles')
+      expect(page).to have_content('Distance is 0.1 mi')
       expect(page).to have_content('Travel Time is 1 min')
       expect(page).to have_content("Head <b>southeast</b> on <b>17th St</b> toward <b>Larimer St</b>")
     end
