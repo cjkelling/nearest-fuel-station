ace76a6d9743d8a57b24831b5070f67df18613a5
Christopher Kelling <cj.kelling88@gmail.com>
2019-12-20 10:50:03 -0700

    Add text to verification test for search results

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -14,7 +14,7 @@ describe "As a user" do
       expect(page).to have_content('Station Name: Seventeenth Street Plaza')
       expect(page).to have_content('Station Address: 1225 17th St.')
       expect(page).to have_content('Station Fuel Type: ELEC')
-      expect(page).to have_content('MO: Not Specified; TU: Not Specified; WE: Not Specified; TH: Not Specified; FR: Not Specified; SA: Not Specified; SU: Not Specified')
+      expect(page).to have_content('Station Access Time: MO: Not Specified; TU: Not Specified; WE: Not Specified; TH: Not Specified; FR: Not Specified; SA: Not Specified; SU: Not Specified')
 
       expect(page).to have_content('Distance is 0.1 mi')
       expect(page).to have_content('Travel Time is 1 min')
